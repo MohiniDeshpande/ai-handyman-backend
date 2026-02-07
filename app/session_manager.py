7f0404c65e81f99b0c76b89e418de399c09382e6
Mohini Deshpande <mohinideshpande003@gmail.com>
2026-02-07 00:53:30 +0000

    Refactor SessionManager to use new variable names

--- app/session_manager.py
+++ app/session_manager.py
@@ -1,34 +1,32 @@
 import asyncio
 import time
 
+
 class SessionManager:
-    def __init__(self, websocket, timeout, warning_time):
-        self.websocket = websocket
+    def __init__(self, ws, timeout, warning):
+        self.ws = ws
         self.timeout = timeout
-        self.warning_time = warning_time
-        self.last_activity = time.time()
-        self.warning_sent = False
+        self.warning = warning
+        self.last_active = time.time()
+        self.warned = False
 
     def touch(self):
-        self.last_activity = time.time()
-        self.warning_sent = False
+        self.last_active = time.time()
+        self.warned = False
 
     async def monitor(self):
         while True:
             await asyncio.sleep(1)
-            elapsed = time.time() - self.last_activity
+            elapsed = time.time() - self.last_active
 
-            if elapsed > self.timeout - self.warning_time and not self.warning_sent:
-                await self.websocket.send_json({
+            if elapsed > self.timeout - self.warning and not self.warned:
+                await self.ws.send_json({
                     "type": "warning",
                     "message": "Session will close in 15 seconds due to inactivity"
                 })
-                self.warning_sent = True
+                self.warned = True
 
             if elapsed > self.timeout:
-                await self.websocket.send_json({
-                    "type": "session_closed",
-                    "reason": "inactivity"
-                })
-                await self.websocket.close()
+                await self.ws.close()
                 break
+
